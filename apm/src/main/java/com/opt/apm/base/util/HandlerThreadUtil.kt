3098eb6c9c1132e3b7243e97cb9dca864cd9d193
ljli <34547492+lihaimings@users.noreply.github.com>
2023-02-21 07:52:06 +0800

    添加HandlerThread通用类

--- apm/src/main/java/com/opt/apm/base/util/HandlerThreadUtil.kt
+++ apm/src/main/java/com/opt/apm/base/util/HandlerThreadUtil.kt
@@ -3,21 +3,74 @@ package com.opt.apm.base.util
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
+import android.util.Log
 import java.util.HashSet
 
 object HandlerThreadUtil {
 
-    @Volatile
-    private var defaultHandlerThread: HandlerThread? = null
+    private const val TAG = "HandlerThread"
+
+    private const val HANDLER_THREAD_NAME = "handler_thread_name"
+
 
     @Volatile
     private var defaultHandler: Handler? = null
 
+    @JvmStatic
     @Volatile
-    private var defaultMainHandler = Handler(Looper.getMainLooper())
+    var defaultHandlerThread: HandlerThread? = null
+        get() {
+            synchronized(HandlerThreadUtil::class.java) {
+                if (null == field) {
+                    field = HandlerThread(HANDLER_THREAD_NAME)
+                    field?.let {
+                        it.start()
+                        defaultHandler = Handler(it.looper)
+                        Log.d(
+                            TAG,
+                            "create default handler thread, we should use these thread norma"
+                        )
+                    }
+                }
+            }
+            return field
+        }
+        private set
 
+    @JvmStatic
+    @Volatile
+    var defaultMainHandler = Handler(Looper.getMainLooper())
 
     private val handlerThreads = HashSet<HandlerThread>()
 
+
+    @JvmStatic
+    fun getDefaultHandler(): Handler? {
+        if (defaultHandler == null) {
+            defaultHandlerThread
+        }
+        return defaultHandler
+    }
+
+    @JvmStatic
+    fun getNewHandlerThread(name: String?, priority: Int): HandlerThread {
+        val i = handlerThreads.iterator()
+        while (i.hasNext()) {
+            val element = i.next()
+            if (!element.isAlive) {
+                i.remove()
+                Log.d(TAG, "warning: remove dead handler thread with name:$name")
+            }
+        }
+        val handlerThread = HandlerThread(name)
+        handlerThread.priority = priority
+        handlerThread.start()
+        handlerThreads.add(handlerThread)
+        Log.d(
+            TAG,
+            "warning: create new handler thread with name $name, alive thread size:${handlerThreads.size}"
+        )
+        return handlerThread
+    }
 
 }
--- apm/src/main/java/com/opt/apm/memory/ActivityRefWatcher.kt
+++ apm/src/main/java/com/opt/apm/memory/ActivityRefWatcher.kt
@@ -1,8 +1,14 @@
 package com.opt.apm.memory
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 
 class ActivityRefWatcher(val application: Application) {
+
+
+
+
 
 
 
